b1ea8a87830e4a71ea36398fe92e0ffb0dec3549
Peter Backx <peter.backx@gmail.com>
2023-07-09 22:40:04 +0200

    Adding vectors to make control easier

--- client.py
+++ client.py
@@ -1,8 +1,7 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2020 Henrik Blidh
-# Copyright (c) 2022 The Pybricks Authors
-
 import asyncio
+import math
+
+import vector
 from bleak import BleakScanner, BleakClient
 
 UART_SERVICE_UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"
@@ -26,14 +25,34 @@ def handle_rx(_, data: bytearray):
     print("Received:", data.decode('utf-8'))
 
 
+async def send(client, rx_char, data):
+    await client.write_gatt_char(rx_char, data)
+
+
+async def send_vector(client, rx_char, vec: vector.Vector2D):
+    if abs(vec.phi) > math.radians(45):
+        print("Phi is too large! Only 45 or below accepted.")
+        return
+
+    print("Sending:", vec.x, vec.y)
+    left = 50
+    right = 50
+    if vec.y > 0:
+        left = int(50 * (vec.x - abs(vec.y)))
+        right = int(50 * (vec.x + abs(vec.y)))
+    elif vec.y < 0:
+        left = int(50 * (vec.x + abs(vec.y)))
+        right = int(50 * (vec.x - abs(vec.y)))
+    left_str = f"{left:02.0f}"
+    right_str = f"{right:02.0f}"
+    print("Sending:", left_str, right_str)
+    await send(client, rx_char, b"D+" + bytes(left_str, "utf-8") + b"+" + bytes(right_str, "utf-8"))
+
+
 async def main():
     # Find the device and initialize client.
     device = await BleakScanner.find_device_by_filter(hub_filter)
     client = BleakClient(device, disconnected_callback=handle_disconnect)
-
-    # Shorthand for sending some data to the hub.
-    async def send(client, data):
-        await client.write_gatt_char(rx_char, data)
 
     try:
         # Connect and get services.
@@ -42,24 +61,21 @@ async def main():
         nus = client.services.get_service(UART_SERVICE_UUID)
         rx_char = nus.get_characteristic(UART_RX_CHAR_UUID)
 
-        # Tell user to start program on the hub.
         print("Start the program on the hub now with the button.")
 
-        await send(client, b"D-50+50")
+        # TODO some kind of delay needed here to wait for the program to start
+
+        await send_vector(client, rx_char, vector.obj(rho=1, phi=0))
         await asyncio.sleep(5)
 
-        await send(client, b"f")
-        await send(client, b"D1")
-        await send(client, b"D-a")
+        await send_vector(client, rx_char, vector.obj(rho=1, phi=math.radians(20)))
         await asyncio.sleep(5)
 
-        await send(client, b"D000000")
-
-        await send(client, b"D+50-50")
+        await send_vector(client, rx_char, vector.obj(rho=1, phi=math.radians(-20)))
         await asyncio.sleep(5)
 
         # Send a message to indicate stop.
-        await send(client, b"B")
+        await send(client, rx_char, b"B")
 
     except Exception as e:
         # Handle exceptions.
@@ -71,4 +87,3 @@ async def main():
 
 # Run the main async program.
 asyncio.run(main())
-
